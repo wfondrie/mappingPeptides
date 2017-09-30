bce10bd835fefcd2109ccecaa45a3adf9fb6b2b5
wfondrie <wfondrie@umaryland.edu>
2017-09-30 08:28:37 -0400

    add a lot of stuff

--- R/mappingPeptides.R
+++ R/mappingPeptides.R
@@ -3,9 +3,26 @@ library(cleaver)
 library(seqinr)
 library(tidyverse)
 
+mapPeptides <- function(fasta, antigen = NULL, minLength = 1, sub = NULL){
+    prot <- toupper(unlist(read.fasta(fasta), as.string = T))[1]
+    
+    mc <- minLength - length(antigen)
+    
+    if(mx < 0) mc <- 0
+    
+    dat <- tibble(pep = unlist(cleave(prot, custom = ".", missedCleavages = mc:nchar(prot))))
+    
+    dat <- dat %>%
+        filter(pep != "") %>%
+        group_by(pep) %>%
+        mutate(mass = MolecularWeight(ConvertPeptide(pep)))
+}
+
 prot <- toupper(unlist(read.fasta("data/triple_KS_Sequenc.fasta", as.string = T)))
 
-dat <- tibble(pep = unlist(cleave(prot, custom = ".", missedCleavages = 100:nchar(prot))))
+prot <- toupper("aklhind")
+
+dat <- tibble(pep = unlist(cleave(prot, custom = "hind", missedCleavages = 0:nchar(prot))))
 
 dat <- dat %>%
     filter(pep != "") %>%
